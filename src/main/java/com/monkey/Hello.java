8b6dcc075ca881f566bf7331a7de9afb3a0358d5
meguni0307 <megunizero@yahoo.com.tw>
2019-01-29 22:23:23 +0800

    Initial commit

---
+++ src/main/java/com/monkey/Hello.java
@@ -0,0 +1,7 @@
+package com.monkey;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/kotlin/com/monkey/Hello.kt
@@ -0,0 +1,5 @@
+package com.monkey
+
+fun main(args: Array<String>) {
+    println("Hello kotlin!")
+}
